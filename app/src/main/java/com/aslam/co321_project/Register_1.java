140d0cfe104ce35a8e8b3349feaae69b786fe2c1
aslam021 <muhammadh.aslamm@gmail.com>
2019-07-17 04:27:59 +0530

    Initial commit

---
+++ app/src/main/java/com/aslam/co321_project/Register_1.java
@@ -0,0 +1,15 @@
+package com.aslam.co321_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aslam.co321_project.R;
+
+public class Register_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_1);
+    }
+}
