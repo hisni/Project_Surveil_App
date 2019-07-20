bd8ead4650eeab39037ca72c3fa7e45a4ed537e1
aslam021 <muhammadh.aslamm@gmail.com>
2019-07-20 11:45:37 +0530

    UI updates

--- app/src/main/java/com/aslam/co321_project/Driver_Home.java
+++ app/src/main/java/com/aslam/co321_project/Driver_Home.java
@@ -6,8 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
 
 public class Driver_Home extends AppCompatActivity {
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,16 @@ public class Driver_Home extends AppCompatActivity {
         setContentView(R.layout.activity_driver__home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /////////// delivery details  //////////
+        lv = findViewById(R.id.deliveries_LV);
+
+        //sample data
+        final String values[] = {"Bio Plus", "Royal", "hi","Bio Plus", "Royal", "hi","Bio Plus", "Royal", "hi","Bio Plus", "Royal", ""};
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Arrays.asList(values));
+        lv.setAdapter(arrayAdapter);
+
+        ///////////////////////////////////////
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
