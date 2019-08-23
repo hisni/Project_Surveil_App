adfc9e3e052ef6479cba14df970efaa6a91bee06
aslam021 <muhammadh.aslamm@gmail.com>
2019-08-23 23:11:00 +0530

    connected to firebase

--- app/src/main/java/com/aslam/co321_project/Register_1.java
+++ app/src/main/java/com/aslam/co321_project/Register_1.java
@@ -2,8 +2,7 @@ package com.aslam.co321_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.aslam.co321_project.R;
+import android.widget.EditText;
 
 public class Register_1 extends AppCompatActivity {
 
@@ -11,5 +10,26 @@ public class Register_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_1);
+
+        final EditText etMail = findViewById(R.id.mail);
+        final EditText etPw = findViewById(R.id.pw);
+        final EditText etPwCnf = findViewById(R.id.pwcnfrm);
+
+
+        if (isValidMail(etMail.getText().toString()) && isValidPW(etPw.getText().toString(), etPwCnf.getText().toString())){
+
+        }
+    }
+
+    final boolean isValidMail(String mail){
+        return true;
+    }
+
+    final boolean isValidPW(String pw1, String pw2){
+        if (pw1.equals(pw2)){
+            return true;
+        }
+
+        return false;
     }
 }
