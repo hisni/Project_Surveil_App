36b835957bef4e939c56bedecc04f9c035787944
aslam021 <muhammadh.aslamm@gmail.com>
2019-08-25 00:50:59 +0530

    error checking updates

--- app/src/main/java/com/aslam/co321_project/Register_1.java
+++ app/src/main/java/com/aslam/co321_project/Register_1.java
@@ -3,6 +3,7 @@ package com.aslam.co321_project;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,15 +18,18 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Register_1 extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    EditText etMail;
+    EditText etPw;
+    EditText etPwCnf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_1);
 
-        final EditText etMail = findViewById(R.id.mailReg);
-        final EditText etPw = findViewById(R.id.pwReg);
-        final EditText etPwCnf = findViewById(R.id.pwcnfrm);
+        etMail = findViewById(R.id.mailReg);
+        etPw = findViewById(R.id.pwReg);
+        etPwCnf = findViewById(R.id.pwcnfrm);
         final Button btnSignUp = findViewById(R.id.signupButton);
         final TextView tvLogIn = findViewById(R.id.hadAccount);
 
@@ -48,8 +52,6 @@ public class Register_1 extends AppCompatActivity {
                                     }
                                 }
                             });
-                } else {
-                    Toast.makeText(Register_1.this, "User name or password wrong", Toast.LENGTH_SHORT).show();
                 }
 
 
@@ -65,15 +67,55 @@ public class Register_1 extends AppCompatActivity {
 
     }
 
-    private boolean isValidMail(String mail){
-        if (mail.length()>0) return true;
-        return false;
+    private boolean isValidMail(String email){
+        if (email.isEmpty()) {
+            etMail.setError("Email is required");
+            etMail.requestFocus();
+            return false;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etMail.setError("Please enter a valid email");
+            etMail.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 
-    private boolean isValidPW(String pw1, String pw2){
-        if (pw1.equals(pw2)){
-            return true;
+    private boolean isValidPW(String password, String password2){
+        if (password.isEmpty()) {
+            etPw.setError("Password is required");
+            etPw.requestFocus();
+            return false;
         }
-        return false;
+
+        if (password.length() < 6) {
+            etPw.setError("Minimum lenght of password should be 6");
+            etPw.requestFocus();
+            return false;
+        }
+
+        if (password2.isEmpty()) {
+            etPwCnf.setError("Please confirm the password");
+            etPwCnf.requestFocus();
+            return false;
+        }
+
+        if (password2.length() < 6) {
+            etPwCnf.setError("Minimum lenght of password should be 6");
+            etPwCnf.requestFocus();
+            return false;
+        }
+
+        if (!password.equals(password2)){
+            etPw.setError("Password should be same");
+            etPw.requestFocus();
+            etPwCnf.setError("Password should be same");
+            etPwCnf.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
